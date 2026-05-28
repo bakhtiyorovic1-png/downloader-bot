3fa1b0c5dfbb651ac4257c7a91df952e515434f3
bakhtiyorovic1-png <bakhtiyorovic1@gmail.com>
2026-05-28 14:39:55 +0500

    Change COOKIES_FILE path to /etc/secrets
    Updated the path for COOKIES_FILE to a secure location.

--- downloader.py
+++ downloader.py
@@ -4,8 +4,7 @@ from ytmusicapi import YTMusic
 
 ytmusic = YTMusic()
 
-COOKIES_FILE = "youtube.com_cookies.txt"
-
+COOKIES_FILE = "/etc/secrets/youtube.com_cookies.txt"
 def detect_platform(url: str) -> str:
     if "youtube.com" in url or "youtu.be" in url:
         return "youtube"
